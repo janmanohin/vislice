1e11324bdd7b2ec999dafa692210484fa768c8ce
Jan Manohin <jan.manohin@gmail.com>
2021-05-03 16:47:50 +0200

    predpriprava na izdelavo spletnega vmesnika

--- model.py
+++ model.py
@@ -4,6 +4,7 @@ PONOVLJENA_CRKA = 'o'
 NAPACNA_CRKA = '-'
 ZMAGA = 'W'
 PORAZ = 'X'
+ZACETEK = 'S'
 
 
 class Igra():
@@ -73,6 +74,29 @@ import random
 def nova_igra():
     geslo = random.choice(bazen_besed)
     return Igra(geslo, []) 
+
+class Vislice:
+
+    def __init__(self):
+        self.igre = {}
+
+    def prost_id_igre(self):
+        if self.igre == {}:
+            return 0
+        else:
+            return max(self.igre.keys()) + 1
+
+    def nova_igra(self):
+        igra = nova_igra()
+        id_igre = self.prost_id_igre()
+        self.igre[id_igre] = (igra, ZACETEK)
+        return id_igre
+
+    def ugibaj(self, id_igre, crka):
+        (igra, _) = self.igre[id_igre]
+        stanje = igra.ugibaj(crka)
+        self.igre[id_igre] = (igra, stanje)
+
 
 # testno_geslo = 'DEŽUJE'
 # testne_crke = ['A', 'I', 'O', 'U', 'D', 'J', 'K']
